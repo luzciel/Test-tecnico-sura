73b07ea3a675a540c9dbb2440c8f96a3a8a1ce54
Luzciel <luzcielmontesinos@gmail.com>
2021-05-13 12:48:04 -0400

    Se agrego el Mensaje de Error

--- src/components/ListCompany.jsx
+++ src/components/ListCompany.jsx
@@ -1,39 +1,53 @@
-import React,  { useState} from "react";
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
 const ListCompany = () => {
-
-    const [dataCompany, setDataCompany] = useState([]);
-    
+  const [dataCompany, setDataCompany] = useState([]);
+  const [erroDiv, setErroDiv] = useState([null]);
+  
   React.useEffect(() => {
-      getData();
+    getData();
   }, []);
 
   const getData = async () => {
     try {
       const data = await axios.get(
         "https://jobs.github.com/positions.json?description=api"
-        );
-        setDataCompany(data.data)
-        console.log(data);
+      );
+      setDataCompany(data.data);
+      console.log(data);
     } catch (error) {
-      console.log(error);
+      setErroDiv(errorData())
     }
   };
-    
-    
-    return (
+  console.log(dataCompany)
+
+  const errorData = () => {
+      return (
+        <div className="error">
+          <h3> No fue posible obtener la data</h3>
+          <p>
+            Verifica tener instalado el plugin de chrome Allow CORS:
+            Access-Control-Allow-Origin.{" "}
+          </p>
+        </div>
+      );
+  };
+
+  return (
     <section className="container-list">
-              {dataCompany.map((item) => (
-                <div key={item.id} className="list-card">
-                  <div className="list-body">
-                      <h5>{ item.title}</h5>
-                      <h5>{item.company}</h5>
-                  </div>
-                    </div>
-              ))}
+      {erroDiv}
+      {dataCompany.map((item) => (
+        <div key={item.id} className="list-card">
+          <div className="list-body">
+            <h5>{item.title}</h5>
+            <h5>{item.company}</h5>
+          </div>
+        </div>
+      ))}
     </section>
   );
 };
 
 export default ListCompany;
+
